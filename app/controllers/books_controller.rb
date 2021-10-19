a7a90e969f8715349de39968c70d238506127cc3
keitsune <tsune.kei.32821@gmail.com>
2021-10-19 14:44:41 +0000

    [Finish] 3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,25 +9,35 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+    else
+      render :index
+    end
   end
 
   private
